4aa2b6990af15205aa007d1f71cfb8efb8094f68
talberts <trinidad.albert98@gmail.com>
2025-05-14 19:33:36 +0200

    first commit, home hecho, medio hecho box preguntas

---
+++ main.js
@@ -0,0 +1,4 @@
+const urlApi = "https://opentdb.com/api.php?amount=10&category=31&difficulty=easy&type=multiple"
+const home = document.getElementById("home")
+const quiz = document.getElementById("quiz")
+const nav = document.getElementById("nav")
