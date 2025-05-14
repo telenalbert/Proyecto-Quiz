32853b821df600b648adfc28ab52e2158a9336d8
talberts <trinidad.albert98@gmail.com>
2025-05-14 20:13:18 +0200

    Terminado CSS final, añadido JS para ir de home a quiz y viceversa

--- main.js
+++ main.js
@@ -1,4 +1,35 @@
 const urlApi = "https://opentdb.com/api.php?amount=10&category=31&difficulty=easy&type=multiple"
+const nav = document.getElementById("nav")
 const home = document.getElementById("home")
 const quiz = document.getElementById("quiz")
-const nav = document.getElementById("nav")
+const final = document.getElementById("final")
+const botonComienzo = document.getElementById("comienzo")
+const botonAtras = document.getElementById("volver")
+const botonSiguiente = document.getElementById("continuar")
+const botonVolveraComenzar = document.getElementById("reinicio")
+const botonHome = document.getElementById("boton-home")
+
+function soloHome (){
+    home.classList.remove("hide")
+    nav.classList.add("hide")
+    quiz.classList.add("hide")
+    final.classList.add("hide")
+}
+
+function soloQuiz () {
+    home.classList.add("hide")
+    nav.classList.remove("hide")
+    quiz.classList.remove("hide")
+    final.classList.add("hide")
+}
+
+function soloFinal () {
+    home.classList.add("hide")
+    nav.classList.remove("hide")
+    quiz.classList.add("hide")
+    final.classList.remove("hide")
+}
+
+botonComienzo.addEventListener("click",soloQuiz)
+botonHome.addEventListener("click",soloHome)
+//botonComienzo.addEventListener("click",soloQuiz)
